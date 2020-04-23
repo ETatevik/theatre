f33556a2f62421a09e217cd31ac76722592e36ae
eTatev <barev.tatevik@gmail.com>
2020-04-24 01:32:14 +0400

    Contact and footer, add js + comp img

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,6 +1,32 @@
 console.clear();
 jQuery(function ($) {
     $(document).ready(function () {
-        
+        // Actors SLIDER
+        {
+        	try {
+        		let swiper = new Swiper('#actors-slider .swiper-container', {
+			      	slidesPerView: 1,
+			      	spaceBetween: 30,
+			      	pagination: {
+			      	  	el: '.swiper-pagination',
+			      	  	clickable: true,
+			      	},
+			      	breakpoints: {
+			      		768:{
+			      			slidesPerView: 2
+			      		},
+                        993: {
+                            slidesPerView: 3
+                        },
+                        1200:{
+                            slidesPerView: 4
+                        }
+                    }
+			    });
+        	} catch(e) {
+        		// statements
+        		console.log(e);
+        	}
+        }
     });
 });
